a63569741a778d57a69bd569e5b877484b47e5ab
silvia22sg <silvia22sg@gmail.com>
2017-09-11 19:58:59 -0300

    Update logical_interpreter.clj

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -1,7 +1,36 @@
 (ns logical-interpreter)
+(defn isFacts [x]
+    (re-find #"^[^\(]*\([^)]*\)$" x)
+)
+(defn isRule [x]
+    (re-find #"^[^\(]*\([^)]*\):-([^\(]*\([^)]*\), *)*([^\(]*\([^)]*\))$" x)
+)
+(defn evaluateFacts [x lista]
+  ;Si el query se encuentra en la listaFacts entonces
+  (println "SI")
+)
+(defn evaluateRule [x]
+)
+(defn generarLista[x lista]
+    (cons x lista)
+)
+(defn search [x list]
+)
+(defn evaluate-query [database query]
+(def listaFacts '())
+(def listaRule '())
+     (def database2 (clojure.string/split database #"\.+"))
+     (doseq [x database2]
+      (def elemento1 (clojure.string/replace x #"(\t|\n)" ""))
+      (def elemento2 (clojure.string/replace elemento1 #"(\t|\s)" ""))
+      (if (isFacts elemento2) (def listaFacts (generarLista elemento2 listaFacts)))
+      (if (isRule elemento2)  (def listaRule (generarLista elemento2 listaRule)))
+     )
+   
+      (def query1 (clojure.string/replace query #"(\t|\n)" ""))
+      (def query2 (clojure.string/replace query1 #"(\t|\s)" "")) 
+     ; (if (isFacts query2) (evaluateFacts query2 listaFacts)) 
+     ; (if (isRule query2) (evaluateRule query2 listaRule)) 
 
-(defn evaluate-query
-  "Returns true if the rules and facts in database imply query, false if not. If
-  either input can't be parsed, returns nil"
-  [database query]
-  nil)
+
+)
